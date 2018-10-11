f9041314b0f111d44b40f6f9db7f90553b5caa9c
luckylion777 <always.smile0915@gmail.com>
2018-10-11 14:45:00 -0300

    build script to download image from online url

--- Scrapy_Lawyers/image_downloader.py
+++ Scrapy_Lawyers/image_downloader.py
@@ -1,18 +1,45 @@
 import csv
-import requests
+import urllib.request
 
 
 def main():
     with open('result.csv') as csvfile:
         csvrows = csv.reader(csvfile, delimiter=',', quotechar='"')
+        line_count = 0
+        count = 0
         for row in csvrows:
-            filename = row[0]
-            url = row[1]
-            print(url)
-            result = requests.get(url, stream=True)
-            if result.status_code == 200:
-                image = result.raw.read()
-                open(filename, "wb").write(image)
+            if line_count == 0:
+                line_count += 1
+            else:
+                filename = row[11].lower() + '_' + row[16].replace('.', '').lower() + '_' + row[13] + '(' + row[19] + ')'
+                profile_url = row[3]
+                firm_url = row[14]
+
+                try:
+                    if not profile_url:
+                        print("This person doesn't have profile image.")
+                    else:
+                        file_type = '.png'
+                        if '.jpg' in profile_url:
+                            file_type = '.jpg'
+                        profile_url = urllib.parse.quote(profile_url, safe=':/')
+                        urllib.request.urlretrieve(profile_url, filename + file_type)
+
+                    if not firm_url:
+                        print("This person doesn't have law firm image.")
+                    else:
+                        if '.jpg' in firm_url:
+                            file_type = '.jpg'
+                        elif '.png' in firm_url:
+                            file_type = '.png'
+                        firm_url = urllib.parse.quote(firm_url, safe=':/')
+                        urllib.request.urlretrieve(firm_url, filename + '_firm' + file_type)
+                    count += 1
+
+                except:
+                    print('ERROR WHILE PARSING IMAGE URL !!!')
+
+                print(count)
 
 
 if __name__ == "__main__":
